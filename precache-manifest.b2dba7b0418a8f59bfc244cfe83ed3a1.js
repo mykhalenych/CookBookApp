83aa6500005d3f7aa6454653b0fc106678cc8483
Mykhalenych <mykhalenych@gmail.com>
2020-07-13 11:27:42 +0300

    Updates

---
+++ precache-manifest.b2dba7b0418a8f59bfc244cfe83ed3a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880d01f4a59e18ddf50310802af11257",
+    "url": "/CookBookApp/index.html"
+  },
+  {
+    "revision": "c49979003ff5d8066695",
+    "url": "/CookBookApp/static/js/2.c9879e8e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CookBookApp/static/js/2.c9879e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d268e55c48558e0ba7e",
+    "url": "/CookBookApp/static/js/main.59fc79c2.chunk.js"
+  },
+  {
+    "revision": "169518e03544b970061e",
+    "url": "/CookBookApp/static/js/runtime-main.ffd395d3.js"
+  }
+]);
